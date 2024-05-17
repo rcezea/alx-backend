cde7d21621aa390cbe22a06483f970f0fecc5105
Richard <rcezea007@gmail.com>
2024-05-17 07:44:55 +0100

    task 2

--- 2-hypermedia_pagination.py
+++ 2-hypermedia_pagination.py
@@ -4,7 +4,8 @@ from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    """ return a tuple of size two containing a start index and an end index """
+    """ return a tuple of size two
+    containing a start index and an end index """
     return (page - 1) * page_size, page * page_size
 
 
