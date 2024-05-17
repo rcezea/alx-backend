e8494e07834cfe8168a0c2e2830c43abe2fc8abc
Richard <rcezea007@gmail.com>
2024-05-17 06:24:46 +0100

    task 0

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" task 0"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ return a tuple of size two containing a start index and an end index """
+    return (page - 1) * page_size, page * page_size
