7567805e2fe8751c086dca042e9b1f006f4d0629
Richard <rcezea007@gmail.com>
2024-05-20 21:56:53 +0100

    task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,9 +5,9 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    stop = page * page_size
-    start = stop - page_size
-    return start, stop
+    """ return a tuple of size two containing
+    a start index and an end index """
+    return (page - 1) * page_size, page * page_size
 
 
 class Server:
