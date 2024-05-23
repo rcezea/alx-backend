574e96281a0ffd99a3e4ff662d6f47240e823b57
Richard <rcezea007@gmail.com>
2024-05-23 05:45:39 +0100

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Extend the base class"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Basic Caching class"""
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if key in self.cache_data.keys():
+                self.cache_data.pop(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            items = list(self.cache_data.items())
+            key, value = items.pop(-2)
+            self.cache_data = dict(items)
+            print(f"DISCARD: {key}")
+
+    def get(self, key):
+        """ Get an item by key """
+        if key and key in self.cache_data:
+            value = self.cache_data[key]
+            self.cache_data.pop(key)
+            self.cache_data[key] = value
+            return self.cache_data[key]
+        return None
