04e9a7adf9f67e4b6c959a75850d5f93b9d18450
Richard <rcezea007@gmail.com>
2024-05-29 05:53:27 +0100

    task 6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -58,7 +58,7 @@ def get_locale() -> Union[str, None]:
 @app.route("/", strict_slashes=False)
 def index() -> str:
     """root path"""
-    return render_template("5-index.html")
+    return render_template("6-index.html")
 
 
 if __name__ == "__main__":
