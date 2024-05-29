1879f08f2349423fe43b91b30bf4ca9cbcf825df
Richard <rcezea007@gmail.com>
2024-05-29 04:16:38 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+""" Basic Flask Application"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index():
+    """The home/index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
